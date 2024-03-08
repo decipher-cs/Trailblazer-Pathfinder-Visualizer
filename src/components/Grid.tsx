693bd3307c5dca74c62f60f14ca45e9904a17161
foobar <uagnl25x@anonaddy.me>
2024-03-08 20:48:44 +0530

    added virtualization to grid using react-window
    Note: Centering the grid is turning out to be quite a challange with the virtualising aspect now in place.

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -4,15 +4,22 @@ import Cell from './Cell'
 import { forwardRef, memo, useRef } from 'react'
 import { useShallow } from 'zustand/react/shallow'
 import { CellType } from '../types'
+import { LazyMotion, domAnimation } from 'framer-motion'
+import { FixedSizeGrid } from 'react-window'
+import AutoSizer from 'react-virtualized-auto-sizer'
+
+const GAP = 4
 
 const Grid = forwardRef((_, ref) => {
-    const { columns, rows } = useGridConfig(useShallow(state => ({ rows: state.rows, columns: state.columns })))
-    const arr = Array.from({ length: rows * columns }, (_, i) => i)
+    const { columns, rows, cellSize } = useGridConfig(
+        useShallow(state => ({ rows: state.rows, columns: state.columns, cellSize: state.cellSize })),
+    )
 
     const cellTypeToPlaceOnClick = useRef<Extract<CellType, 'start' | 'finish'>>('start')
 
     return (
         <Box
+            ref={ref}
             sx={{
                 borderImage: 'linear-gradient(45deg, red, blue)',
                 borderImageSlice: 1,
@@ -26,30 +33,34 @@ const Grid = forwardRef((_, ref) => {
 
                 overflow: 'hidden',
                 p: 3,
+                width: '100%',
+                height: '100%',
             }}
         >
-            <Box
-                ref={ref}
-                sx={{
-                    display: 'grid',
-
-                    p: 1,
-                    gap: 0.3,
-
-                    gridTemplateRows: `repeat(${rows}, min-content)`,
-                    gridTemplateColumns: `repeat(${columns}, min-content)`,
-
-                    maxWidth: '100%',
-                    maxHeight: '100%',
-                    width: 'fit-content',
-                    height: 'fit-content',
-                    overflow: 'auto',
-                }}
-            >
-                {arr.map(i => (
-                    <Cell key={i} index={i} cellTypeToPlaceOnClick={cellTypeToPlaceOnClick} />
-                ))}
-            </Box>
+            <LazyMotion features={domAnimation}>
+                <AutoSizer>
+                    {({ height, width }) => (
+                        <FixedSizeGrid
+                            style={{ overflow: 'auto', transform: 'translate(-50%, -50%)' }}
+                            columnCount={columns}
+                            rowCount={rows}
+                            width={width}
+                            height={height}
+                            columnWidth={cellSize + GAP}
+                            rowHeight={cellSize + GAP}
+                        >
+                            {props => (
+                                <div style={{ ...props.style }}>
+                                    <Cell
+                                        index={props.rowIndex * columns + props.columnIndex}
+                                        cellTypeToPlaceOnClick={cellTypeToPlaceOnClick}
+                                    />
+                                </div>
+                            )}
+                        </FixedSizeGrid>
+                    )}
+                </AutoSizer>
+            </LazyMotion>
         </Box>
     )
 })
